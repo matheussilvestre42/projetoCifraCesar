d57e472dacd4fbe77d3af4a9c666356dfb38f873
matheussilvestre42 <98432355+matheussilvestre42@users.noreply.github.com>
2022-04-02 21:40:04 -0300

    atualizando o projeto

--- js/code.js
+++ js/code.js
@@ -1,9 +1,10 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 function encrypt() {
-    let text = document.querySelector('.textCrypto').value
+    //catch the elements write by the user
+    let text = document.querySelector('.textCrypto').value.split('')
     let key = Number(document.querySelector('.key').value)
+    
     let newText = ''
-    text = text.split('')
     for(let i = 0; i < text.length; i++) {
         if(text[i] == ' ') newText += text[i]
         for(let j = 0; j < alphabet.length; j++){
@@ -22,5 +23,11 @@ function encrypt() {
         }
     }
     document.querySelector('.textDecrypto').value = newText
+}
+
+function decrypt(){
+    let cryptText = document.querySelector('.textDecrypto').value.split('')
+    let key = Number(document.querySelector('.key').value)
+    let textDecrypt = ''
 }
 
