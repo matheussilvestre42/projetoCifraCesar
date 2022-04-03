23131af5f27df05072a9dd5e8985fc94266cfe65
matheussilvestre42 <98432355+matheussilvestre42@users.noreply.github.com>
2022-04-03 00:05:00 -0300

    projetoCifraCesar v 1.1
    Melhorando HTML e JS
    Adicionando CSS

--- js/code.js
+++ js/code.js
@@ -1,7 +1,7 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 function encrypt() {
     //catch the elements write by the user
-    let text = document.querySelector('.textCrypto').value.split('')
+    let text = document.querySelector('.textCrypto').value.toLowerCase().split('')
     let key = Number(document.querySelector('.key').value)
     
     let newText = ''
@@ -9,6 +9,11 @@ function encrypt() {
         if(text[i] == ' ') newText += text[i]
         for(let j = 0; j < alphabet.length; j++){
             if(text[i] == alphabet[j]){
+                /*
+                *This section changes the key to suit the 26 different combinations
+                *Thus creating 26 combinations of the text with the limit key 150
+                *After this limit possibly some character will return as undefined
+                 */
                 if(key > 26) if(key > 52) {
                     if(key > 78){
                         if(key > 104){
@@ -16,6 +21,7 @@ function encrypt() {
                         }else key -= 78
                     }else key -= 52
                 }else key -= 26
+
                 newText += alphabet[j + key]
                 break
             }
@@ -26,8 +32,29 @@ function encrypt() {
 }
 
 function decrypt(){
-    let cryptText = document.querySelector('.textDecrypto').value.split('')
+    let cryptText = document.querySelector('.textCrypto').value.toLowerCase().split('')
     let key = Number(document.querySelector('.key').value)
     let textDecrypt = ''
+    for(let i = 0; i < cryptText.length; i++) {
+        if(cryptText[i] == ' ') textDecrypt += cryptText[i]
+        for(let j = 0; j < alphabet.length; j++) {
+            if(cryptText[i] == alphabet[j]){
+
+                if(key > 26) if(key > 52) {
+                    if(key > 78){
+                        if(key > 104){
+                            key -= 104
+                        }else key -= 78
+                    }else key -= 52
+                }else key -= 26
+
+                textDecrypt += alphabet[(j+26) - key]
+                break
+            }
+
+        }
+    }
+
+    document.querySelector('.textDecrypto').value = textDecrypt
 }
 
